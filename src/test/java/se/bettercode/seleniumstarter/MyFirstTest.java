fc3179308afa632af9c71951aaa0f84789f5fa84
Max Wenzin <max.wenzin@gmail.com>
2016-11-11 21:43:06 +0100

    Simplified MyFirstTest

--- src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
+++ src/test/java/se/bettercode/seleniumstarter/MyFirstTest.java
@@ -1,7 +1,5 @@
 package se.bettercode.seleniumstarter;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -13,22 +11,12 @@ import static org.junit.Assert.assertEquals;
  */
 public class MyFirstTest {
 
-  private WebDriver driver;
-
-  @Before
-  public void setUp() {
-    driver = new HtmlUnitDriver();
-  }
-
-  @After
-  public void cleanUp() {
-    driver.quit();
-  }
-
   @Test
   public void goToWebPage() {
+    WebDriver driver = new HtmlUnitDriver();
     driver.get("http://the-internet.herokuapp.com");
     assertEquals("The Internet", driver.getTitle());
+    driver.quit();
   }
 
 }
